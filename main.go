74fac1358ef2fbe124a354186d387d93d5fd0c3f
Ben Liu <ben.liu@tabcorp.com.au>
2018-02-07 11:59:18 +1100

    Update model

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 
 // Todo item structure
 type Todo struct {
-	Name      string
-	Completed bool
-	Due       time.Time
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
 }
 
 // Todos type is list of todo
